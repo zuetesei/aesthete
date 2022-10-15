f8d5c381a43b2d973cc0b1f134d577fda76be89a
zue <zuleikatesei@gmail.com>
2022-10-15 15:18:25 -0700

    added login template and styles

--- controllers/index.js
+++ controllers/index.js
@@ -7,10 +7,10 @@ router.use('/home', homeRoutes);
 router.use('/api', apiRoutes);
 router.use('/dashboard', dashboardRoutes);
 
-router.get('/',(req, res) => {
+router.get('/', (req, res) => {
     // res.status(404).end();
     res.send('running')
-    
+
 });
 
 module.exports = router;

a47abe33375ddae76b348ae4925fe150776aa4ba
Dennis Jackson <dennisdjack1987@gmail.com>
2022-10-16 21:10:44 -0700

    add route redirects

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -5,6 +5,10 @@ const { User, Image } = require('../models');
 
 router.route('/')
     .get((req, res) => {
+        if(req.session.loggedIn){
+            res.redirect('/dashboard')
+            return
+        }
         res.render('homepage')
     })
 // (1) user not logged in, render homepage 
